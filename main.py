f747809dc16774f9c2c5aff84230f9fc6b353978
CammN <cameron.nuttall@gmail.com>
2025-12-02 13:49:15 +0000

    Initial repository setup and structuring

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from ev-load-fc!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/ev_load_fc/test.py
@@ -0,0 +1,7 @@
+class test_class:
+
+    def __init__(self):
+        self.name = 'cam'
+
+    def get_name(self):
+        print(self.name)
